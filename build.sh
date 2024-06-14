10053d0af898a4383bb3b00a0b368edbaf97f5b6
Arthur Ross <arthurross@live.nl>
2024-06-14 10:10:54 +0200

    look at me now

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Update package list and install OpenSSL
-apt-get update
-apt-get install -y libssl-dev
-
 # Ensure Bundler is installed
 gem install bundler
 
 # Install the required gems
 bundle install
 
-# Run the build command
-npm run build
+# Run the Jekyll build
+bundle exec jekyll build
 
