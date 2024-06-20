88c5cb5f10685ceb8e20bbf8c362ee56789c9340
Arthur van der Loon <vanderloonarthur@gmail.com>
2024-06-20 11:10:54 +0200

    add render

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .models import Feedback
 import json
