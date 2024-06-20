c0def676dffb9b42820b354476a0e97137910a52
Arthur van der Loon <vanderloonarthur@gmail.com>
2024-06-20 11:03:00 +0200

    commit models

--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,10 @@
-# models.py
 from django.db import models
 
 class Feedback(models.Model):
-    feedback = models.TextField()
-    reaction = models.CharField(max_length=255)
+    feedback = models.CharField(max_length=255)
+    reaction = models.CharField(max_length=50)
     additional_comments = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.feedback
--- gallery/views.py
+++ gallery/views.py
@@ -2,7 +2,9 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Feedback
 import json
-from django.shortcuts import render
+import logging
+
+logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def receive_feedback(request):
@@ -15,6 +17,7 @@ def receive_feedback(request):
 
             # Save data to the database
             try:
+                logger.info(f"Received feedback: {feedback}, reaction: {reaction}, additional_comments: {additional_comments}")
                 Feedback.objects.create(
                     feedback=feedback,
                     reaction=reaction,
@@ -22,13 +25,14 @@ def receive_feedback(request):
                 )
                 return JsonResponse({'message': 'Feedback received and stored successfully'})
             except Exception as e:
-                print("Error saving feedback:", e)
+                logger.error(f"Error saving feedback: {e}")
                 return JsonResponse({'error': 'Error saving feedback'}, status=500)
         except Exception as e:
-            print("Error parsing request data:", e)
+            logger.error(f"Error parsing request data: {e}")
             return JsonResponse({'error': 'Invalid request data'}, status=400)
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=405)
+
 
 def feedback_list(request):
     feedbacks = Feedback.objects.all()
