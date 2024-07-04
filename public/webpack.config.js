1952c2c2f15ec6ab262e9979757b8485d73b06b0
Arthur van der Loon <vanderloonarthur@gmail.com>
2024-07-04 15:58:57 +0200

    public push

---
+++ public/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './assets/js/_main.js', // Adjust entry point as per your project structure
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv()
+  ],
+};
